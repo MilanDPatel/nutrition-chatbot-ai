ae5ae73e1260e065888b35e3972e971132aaa752
Milan Patel <patelmilan2005@gmail.com>
2025-01-08 16:10:44 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Nutrition AI Project',
+    version= '0.0.0',
+    author= 'Milan Patel',
+    author_email= 'patel.d.milan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
